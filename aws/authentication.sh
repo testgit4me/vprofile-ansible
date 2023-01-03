193522d0393f01639239bd80a2341116eb0f4a4d
workuser <workuser777@gamil.com>
2023-01-03 13:36:42 +0500

    commit

--- aws/authentication.sh
+++ aws/authentication.sh
@@ -4,19 +4,15 @@ source .bashrc
 apt install python3-pip -y
 pip install boto3 -y
 pip install boto -y
-
 ansible-playbook aws.yaml
 
 
-
-
-
-              - name: start multiple instances
-      ec2_instance:
-        key_name: "keypair"
-        instance_type: t2.micro
-        image_id: ami-0574da719dca65348
-        count: 1
-        region: us-east-1
-        tags:
-          foo: bar
+- name: start multiple instances
+    ec2_instance:
+    key_name: "keypair"
+    instance_type: t2.micro
+    image_id: ami-0574da719dca65348
+    count: 1
+    region: us-east-1
+    tags:
+    foo: bar
